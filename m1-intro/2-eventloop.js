daa3a9db39d851bb7678a130855a3d7468048904
Jatin Sanghvi <jatin085@yahoo.com>
2015-08-08 16:57:29 +0000

    Fixed style issues reported by JSLint

--- m1-intro/2-eventloop.js
+++ m1-intro/2-eventloop.js
@@ -1,15 +1,19 @@
-var count = 0;
+(function () {
+    'use strict';
 
-var t = setInterval(function() {
-    count++;
-    console.log("Interval of 10 ms, count = " + count);
-    if (count === 10) {
-        clearInterval(t);
-    }
-}, 10);
+    var count = 0;
 
-setTimeout(function() {
-    console.log("Timeout at 200 ms");
-},200);
+    var t = setInterval(function () {
+        count += 1;
+        console.log("Interval of 10 ms, count = " + count);
+        if (count === 10) {
+            clearInterval(t);
+        }
+    }, 10);
 
-console.log("Running!");
+    setTimeout(function () {
+        console.log("Timeout at 200 ms");
+    }, 200);
+
+    console.log("Running!");
+}());
--- m1-intro/3-callback.js
+++ m1-intro/3-callback.js
@@ -1,51 +1,54 @@
-var maxTime = 1000;
+(function () {
+    'use strict';
 
-// If input is even, double it
-// If input is odd, error
-// (call takes random amount of time < 1s)
-var evenDoubler = function(v, callback) {
-    var waitTime = Math.floor(Math.random()*(maxTime+1));
-    if (v%2) {
-        setTimeout(function() {
-            callback(new Error("Odd input"));
-        }, waitTime);
-    } else {
-        setTimeout(function() {
-            callback(null, v*2, waitTime);
-        }, waitTime);
-    }
-};
+    var maxTime = 1000;
 
-// Version 1:  Named function to process results:
+    // If input is even, double it
+    // If input is odd, log error
+    // (call takes random amount of time < 1s)
+    var evenDoubler = function (v, callback) {
+        var waitTime = Math.floor(Math.random() * maxTime + 1);
+        if (v % 2 !== 0) {
+            setTimeout(function () {
+                callback(new Error("Odd input"));
+            }, waitTime);
+        } else {
+            setTimeout(function () {
+                callback(null, v * 2, waitTime);
+            }, waitTime);
+        }
+    };
 
-// var processResults = function(err, results, time) {
-//     if (err) {
-//         console.log("ERROR: " + err.message);
-//     } else {
-//         console.log("The results are: " + results + " (" + time + " ms)");
-//     }
-// };
+    // Version 1:  Named function to process results:
 
-// evenDoubler(4, processResults);
-// evenDoubler(5, processResults);
+    // var processResults = function(err, results, time) {
+    //     if (err) {
+    //         console.log("ERROR: " + err.message);
+    //     } else {
+    //         console.log("The results are: " + results + " (" + time + " ms)");
+    //     }
+    // };
+
+    // evenDoubler(4, processResults);
+    // evenDoubler(5, processResults);
 
 
-// Version 2:  Use anonymous function and track callbacks to print "Done!" message
+    // Version 2:  Use anonymous function and track callbacks to print "Done!" message
 
-var count = 0;
-
-for (var i = 0; i<10; i++) {
-    console.log("Calling evenDoubler for value: " + i);
-    evenDoubler(i, function(err, results, time) {
+    var processResults = function (err, results, time) {
         if (err) {
             console.log("ERROR: " + err.message);
         } else {
             console.log("The results are: " + results + " (" + time + " ms)");
         }
-        if (++count === 10) {
-            console.log("Done!");
-        }
-    });
-};
+    };
 
-console.log("-----");
+    var i = 0;
+    for (i = 0; i < 10; i += 1) {
+        console.log("Calling evenDoubler for value: " + i);
+        evenDoubler(i, processResults);
+    }
+
+    console.log("Done!");
+    console.log("-----");
+}());
